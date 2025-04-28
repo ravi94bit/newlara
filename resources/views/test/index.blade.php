41d5124bee4a83ea61f3fb4ebf886fee7f496ef3
RaviSingh <Ravi.singh@richestsoft.in>
2025-04-28 13:08:31 +0530

    button new

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -48,6 +48,9 @@
 
                     <button type="submit" class="btn btn-primary w-100">Submit</button>
                     <button type="submit" class="btn btn-primary w-100">Submit</button>
+                    <button type="submit" class="btn btn-primary w-100">Submit</button>
+                    <button type="submit" class="btn btn-primary w-100">Submit</button>
+                    <button type="submit" class="btn btn-primary w-100">Submit</button>
                 </form>
             </div>
         </div>
